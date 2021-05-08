16db40ff745163dffb3091dd2059751f53dce996
Martin <mn.vichev@gmail.com>
2021-05-08 10:29:17 +0200

    Added basic API functionality

--- mysite/app.py
+++ mysite/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from models import create_post, get_posts
+import json
 
 app = Flask(__name__)
 
@@ -17,6 +18,30 @@ def index():
     posts = get_posts()
 
     return render_template('index.html', posts=posts)
+
+@app.route('/posts', methods=['GET'])
+def getAllPosts():
+
+	# if request.method == 'GET':
+    #     pass
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        post = request.form.get('post')
+        create_post(name, post)
+
+    posts = get_posts()
+    posts_l = []
+    posts_d = {}
+    for post in posts:
+        posts_d["id"] = post[0]
+        # posts_d["user_id"] = post[0]
+        posts_d["username"] = post[1]
+        posts_d["content"] = post[2]
+        posts_d["likes"] = post[3]
+        posts_l.append(posts_d)
+    jsonString = json.dumps(posts_l)
+    return jsonString
 
 if __name__ == '__main__':
     app.run(debug=True)
